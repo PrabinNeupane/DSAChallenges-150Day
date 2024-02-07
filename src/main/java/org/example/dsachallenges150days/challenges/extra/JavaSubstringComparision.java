c33517207a0fdc44a0b17e6da2fc0fb9d965fb0c
prabin693 <neupaneprabin693@gmail.com>
2024-02-07 23:09:31 +0545

    added basic Mean calculation

--- src/main/java/org/example/dsachallenges150days/challenges/extra/JavaSubstringComparision.java
+++ src/main/java/org/example/dsachallenges150days/challenges/extra/JavaSubstringComparision.java
@@ -27,4 +27,16 @@ public class JavaSubstringComparision {
         }
         return smallest + "\n" + largest;
     }
+
+    public static int medianCalculate(int a, int b) {
+        int result;
+        if (a < b) {
+            result = a + (b - a) / 2;
+        } else {
+            result = a + (a - b) / 2;
+        }
+        return result;
+    }
+
 }
+
