7b55d144c709a722400ac7509cf45f44d07fbd63
prabinneupane <neupaneprabin693@gmail.com>
2024-05-14 23:57:08 +0545

    monotonic sort

---
+++ src/main/java/org/example/dsachallenges150days/Solution.java
@@ -0,0 +1,42 @@
+package org.example.dsachallenges150days;
+
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1, 7, 9, 5};
+
+        Arrays.sort(nums);
+
+        int[] result = monotonicStack(nums);
+        System.out.print("Monotonic increasing stack: [");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+            if (i != result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
+    }
+
+    private static int[] monotonicStack(int[] nums) {
+        Deque<Integer> monotonicStack = new ArrayDeque<>();
+        for (int num : nums) {
+            System.out.println(monotonicStack.peekLast());
+            while (!monotonicStack.isEmpty() && monotonicStack.peekLast() > num) {
+                monotonicStack.pollLast();
+            }
+            monotonicStack.offerLast(num);
+        }
+
+        int[] result = new int[monotonicStack.size()];
+        int index = monotonicStack.size() - 1;
+        while (!monotonicStack.isEmpty()) {
+            result[index--] = monotonicStack.pollLast();
+        }
+        return result;
+    }
+}
