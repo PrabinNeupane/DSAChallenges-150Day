e6a0b2143149d64c24c6779ba4f68e4afe1f5da3
prabinneupane <neupaneprabin693@gmail.com>
2024-05-15 11:53:05 +0545

    reversed a string

--- src/main/java/org/example/dsachallenges150days/ReverseWordInString.java
+++ src/main/java/org/example/dsachallenges150days/ReverseWordInString.java
@@ -1,9 +1,6 @@
 package org.example.dsachallenges150days;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ReverseWordInString {
@@ -13,11 +10,43 @@ public class ReverseWordInString {
         System.out.println(result);
     }
 
+//    public static String reverseWords(String s) {
+//        List<String> words = Arrays.stream(s.trim().split("\\s+"))
+//                .collect(Collectors.toList());
+//        Collections.reverse(words);
+//        return String.join(" ", words);
+//    }
+
+
+    //use double pointer to get response like above method
     public static String reverseWords(String s) {
-        List<String> words = Arrays.stream(s.trim().split("\\s+"))
-                .collect(Collectors.toList());
-        Collections.reverse(words);
-        return String.join(" ", words);
+        int left = 0;
+        int right = s.length() - 1;
+        // remove leading spaces
+        while (left <= right && s.charAt(left) == ' ') ++left;
+
+        // remove trailing spaces
+        while (left <= right && s.charAt(right) == ' ') --right;
+
+        Deque<String> d = new ArrayDeque();
+        StringBuilder word = new StringBuilder();
+
+        // push word by word in front of deque
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if ((word.length() != 0) && (c == ' ')) {
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+
+        return String.join(" ", d);
     }
+
 }
 
