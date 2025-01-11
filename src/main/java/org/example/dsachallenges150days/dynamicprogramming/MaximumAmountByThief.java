40b68c430e2fedc1f54d5310ef11b06b7e533360
Prabin Neupane <neupaneprabin693@gmail.com>
2025-01-11 22:18:27 +0545

    solved a problem using dynamic programming for robbing a house with a maximum amount

--- src/main/java/org/example/dsachallenges150days/dynamicprogramming/MaximumAmountByThief.java
+++ src/main/java/org/example/dsachallenges150days/dynamicprogramming/MaximumAmountByThief.java
@@ -20,7 +20,7 @@ public class MaximumAmountByThief {
         dp[1] = Math.max(nums[0], nums[1]);
 
         for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(dp[i - 1] + nums[i], dp[i - 2] + nums[i]);
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
 
         }
         return dp[nums.length - 1];
